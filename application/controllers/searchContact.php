c6f3f8cb635cbb94b482e025410cfe2b9195eb40
AnirudhMadedhi <anirudhmadedhi@gmail.com>
2019-12-22 20:29:42 +0530

    Second

--- application/controllers/searchContact.php
+++ application/controllers/searchContact.php
@@ -31,33 +31,6 @@ class searchContact extends REST_Controller {
 	{
         $name=$this->input->post('name');
         $final=$this->contact_search->search($name);
-        // $data = $this->db->where("contacts", ['first_name' => $name])->get();
-        // $data2 = $this->db->select('*')->from('contacts')
-        // ->group_start()
-        //         ->where('first_name', $name)
-        //         ->or_group_start()
-        //                 ->where('last_name', $name)
-        //         ->group_end()
-        // ->group_end()->get();
-          //  $data2=$this->db->query("select email from contacts where first_name='".$name."' or last_name='".$name."'");
-        //   function get_search($example)
-        //   {
-        //         $this->db->from('contacts');
-        //       $this->db->where('first_name=', $example);
-        //       $this->db->or_where('last_name', $example);
-        //     //   $this->db->or_like('Plaats', $match);
-        //     //   $this->db->or_like('Telefoonnummer', $match);
-        //     //   $this->db->or_like('Email', $match);
-        //     //   $this->db->or_like('Website', $match);
-        //     //   $this->db->or_like('Profiel', $match);
-        //     //   $this->db->or_like('Adres', $match);
-        //       $query = $this->db->get();
-          
-        //       return $query->result();
-        //   }
-
-        //   $data3=get_search($name);
-     
         $this->response($final, REST_Controller::HTTP_OK);
     }
         
